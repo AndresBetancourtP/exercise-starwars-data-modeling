bae18ca0d407e20d69147bb1e55f3d1e46101025
AndresBetancourtP <betancourt.andres10@gmail.com>
2023-01-05 16:19:42 +0000

    Revisado favoritos

--- src/models.py
+++ src/models.py
@@ -8,43 +8,68 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = 'user'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    user_id = Column(Integer, primary_key=True)
-    user_name = Column(String(80), unique=True, nullable=False)
-    first_name =Column(String(80), nullable=False)
-    last_name =Column(String(80), nullable=False)
-    created_at =Column(String(80))
-    updated_at =Column(String(80))
-    email =Column(String(80),unique=True, nullable=False) 
+class Users (Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    lastname = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+    # date_subs = Column(DateTime, nullable=False)
 
-class Planet(Base):
-    __tablename__ = 'planet'
-    planet_id = Column(Integer, primary_key=True)
-    name = Column(String(200))
-    climate = Column(String(200))
-    population = Column(Integer)
-    orbital_period = Column(Integer)
-    diameter= Column(Integer)
 
-class Character(Base):
-    __tablename__ = 'character'
-    character_id = Column(Integer, primary_key=True)
-    name =  Column(String(200))
-    birth_year = Column(Integer)
-    gender = Column(String(200))
-    height = Column(Integer)
-    skin_color = Column(String(200))
-    eye_color = Column(String(200))
+class Characters (Base): 
+    __tablename__ = "characters"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    birthday_year = Column(Integer, nullable=False)
+    Gender = Column(String(6), nullable=False)
+    Height = Column(Integer, nullable=False)
+    Skin_color = Column(String(200), nullable=False)
+    Eye_color = Column(String(200), nullable=False)
 
-class Favorites(Base): 
-    __tablename__ = 'favorites'
-    like_id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('user.user_id'))
-    fav_planet_id = Column(Integer, ForeignKey('planet.planet_id'))
-    fav_character_id = Column(Integer, ForeignKey('character.character_id'))
+
+class Planets (Base):
+    __tablename__ = "planets"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    diameter = Column(Integer, nullable=False)
+    rotation_period = Column(Integer, nullable=False)
+    orbital_period = Column(Integer, nullable=False)
+
+
+class Vehicles (Base):
+    __tablename__ = "vehicles"
+    id = Column(Integer, primary_key=True)
+    model = Column(String(250), nullable=False)
+    vehicle_class = Column(String(250), nullable=False)
+    manufacturer = Column(String(250), nullable=False)
+    length = Column(Integer, nullable=False)
+
+
+class Fav_Characters (Base):
+    __tablename__ = "fav_characters"
+    id = Column(Integer, primary_key=True)
+    user_id  = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)
+    character_id = Column(Integer, ForeignKey('characters.id'))
+    character = relationship(Characters)
+
+class Fav_Planets (Base):
+    __tablename__ = "fav_planets"
+    id = Column(Integer, primary_key=True)
+    user_id  = Column(Integer, ForeignKey('user.id'))
+    user = relationship(Users)
+    planet_id = Column(Integer, ForeignKey('planets.id'))
+    planet = relationship(Planets)
+
+class Fav_Vehicles (Base):
+    __tablename__ = "fav_vehicles"
+    id = Column(Integer, primary_key=True)
+    user_id  = Column(Integer, ForeignKey('users.id'))
+    user = relationship(Users)
+    vehicle_id = Column(Integer, ForeignKey('vehicles.id'))
+    vehicle = relationship(Vehicles)
 
     def to_dict(self):
         return {}
